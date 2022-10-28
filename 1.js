032172c4e7abd9200956e385af1b68466097668a
Ekyd <Ekyd777@gmail.com>
2022-10-28 10:09:50 +0300

    add homework

---
+++ 1.js
@@ -0,0 +1,3 @@
+let faringate = prompt("Введите темпераутру в градусах Фарингейта")
+let celsii = Math.round((faringate - 32) * (9/5))
+alert("Цельсий: " + celsii + "\n" + "Фарингейт: " + faringate)
